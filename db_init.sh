9faf82b0d5051f2405110504131caa8fbcf5ed9c
pswaminath <padmapriyaswaminathan@gmail.com>
2024-05-28 10:58:23 -0500

    Modify database initialization script and add ro to trials folder

--- db_init.sh
+++ db_init.sh
@@ -1,4 +1,6 @@
-for f in $(<listNCT.txt)
+#for f in $(<listNCT.txt)
+for f in /data/trials/*.full.ndjson
 do
-mongoimport --db aci --collection ClinicalTrials --type json --file /path/to/trialsfolder/$f --jsonArray
+#mongoimport --db aci --collection ClinicalTrials --type json --file /path/to/trialsfolder/$f --jsonArray
+mongoimport --db aci --collection ClinicalTrials --type json --file $f --jsonArray
 done
